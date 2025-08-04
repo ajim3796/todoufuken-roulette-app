221a8cf3586422190c27a362dd02ed59b376a5ec
ajim <n1a1o2k0i@gmail.com>
2025-08-04 19:54:51 +0900

    update

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ imageElements[topImageIndex].style.display = "";
 
 // チェックボックス
 const checkboxes = document.querySelectorAll("input[type=checkbox]");
-let perfecture = [1, 19, 24, 28, 44];
+let perfecture = [1, 24, 28, 44];
 perfecture.forEach((index) => {
   checkboxes[index].checked = false;
 });
